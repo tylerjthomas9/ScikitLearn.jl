4fa14afa20836fa24dd1c5d67d3358c5e051794b
tylerjthomas9 <tyler@saguarocm.com>
2023-05-31 10:24:39 -0700

    limit CI, try adding openblas

--- src/Skcore.jl
+++ src/Skcore.jl
@@ -195,7 +195,7 @@ function import_sklearn()
                 end
                 # force reinstall of scikit-learn replacing any previous mkl version
                 Conda.add("scikit-learn>=1.2,<1.3", channel="conda-forge")
-                #Conda.add("openblas")
+                Conda.add("openblas")
                 #Conda.add("llvm-openmp", channel = "conda-forge")
                 mkl_checked = true
             end
@@ -203,12 +203,12 @@ function import_sklearn()
         else
             @static if Sys.islinux()
                 if !libstdcxx_solved
-                version = _compatible_libstdcxx_ng_version()
-                if version !== nothing
-                    Conda.add("conda", channel="anaconda")
-                    Conda.add("libstdcxx-ng$version", channel="conda-forge")
-                end
-                libstdcxx_solved = true
+                    version = _compatible_libstdcxx_ng_version()
+                    if version !== nothing
+                        Conda.add("conda", channel="anaconda")
+                        Conda.add("libstdcxx-ng$version", channel="conda-forge")
+                    end
+                    libstdcxx_solved = true
                 end
             end
         end
